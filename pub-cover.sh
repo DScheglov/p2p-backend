b828fb483dd600ccca5532f65ffd423132889194
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2016-04-08 14:16:59 +0300

    "changes 2016-04-08 14:16:59"

--- pub-cover.sh
+++ pub-cover.sh
@@ -3,8 +3,8 @@ NOW=$(date +"%Y-%m-%d %H:%M:%S")
 MASTER_COMMIT_MSG="changes $NOW"
 PAGES_COMMIT_MSG="coverage $NOW"
 
-TMP_DIR="cvrg"
-CVRG_DIR="coverage"
+TMP_DIR="./cvrg"
+CVRG_DIR="./coverage"
 
 rm $TMP_DIR -rf
 git add .
@@ -12,12 +12,12 @@ git commit -m '"'"$MASTER_COMMIT_MSG"'"'
 git push origin master
 
 mkdir $TMP_DIR
-mv "$CVRG_DIR/*" $TMP_DIR
+mv "$CVRG_DIR"/* $TMP_DIR
 git checkout gh-pages
 
-cp "$TMP_DIR/*" $CVRG_DIR -rf
+cp "$TMP_DIR"/* $CVRG_DIR -rf
 rm $TMP_DIR -rf
-git add "$CVRG_DIR/." -A
+git add "$CVRG_DIR"/. -A
 git commit -m '"'"$PAGES_COMMIT_MSG"'"'
 git push origin gh-pages
 git checkout master
